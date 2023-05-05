03b1dbe97cc959f7d937fd780105885ca5ad2c03
N1kolay <73244584+n1kolaysk1ba@users.noreply.github.com>
2023-05-05 21:07:02 +0300

    Add files via upload

---
+++ Lab-5/ExchangeRates.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    public class ExchangeRates
+    {
+        public string Base { get; set; }
+        public string Date { get; set; }
+        public Rates Rates { get; set; }
+    }
+
+    public class Rates
+    {
+        public float USD { get; set; }
+        public float EUR { get; set; }
+    }
+}
