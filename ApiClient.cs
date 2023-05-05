bdd90dce4ce064f4f9720d5ebcac2bd8b2e09c03
N1kolay <73244584+n1kolaysk1ba@users.noreply.github.com>
2023-05-05 21:01:53 +0300

    Lab - 5

---
+++ ApiClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp5
+{
+    public class ApiClient
+    {
+        private const string BaseUrl = "https://api.apilayer.com/exchangerates_data/live?base=USD&symbols=EUR,GBP";
+
+        private readonly HttpClient _client;
+
+        public ApiClient(string apiKey)
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new System.Uri(BaseUrl);
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Add("User-Agent", "ApiClient");
+            _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+        }
+
+        public HttpClient GetClient()
+        {
+            return _client;
+        }
+    }
+}
---
+++ ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace ConsoleApp5
+{
+    public class ApiResponse<T>
+    {
+        public string Message { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public List<T> Data { get; set; }
+    }
+}
